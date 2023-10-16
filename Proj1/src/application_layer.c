a7bcf158631c81fbdd1e6ab5a9bdf8c5652b3c77
Rúben Silva <rubensilva2003@gmail.com>
2023-10-16 22:16:56 +0100

    Changes to app_layer.c - made app_layer.h the same as the base code given, since it says do not change

--- Proj1/src/application_layer.c
+++ Proj1/src/application_layer.c
@@ -4,58 +4,19 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include "link_layer.h"
+
+struct applicationLayer{
+int fileDescriptor; /*Descritor correspondente à porta série*/
+int status; /*TRANSMITTER | RECEIVER*/
+};
 
 #define END_PACKET 0x03
 #define START_PACKET 0x02
 #define DATA_PACKET 0x01
-
-void applicationLayer(const char *port, const char *role, int baudRate,
-                      int retries, int timeout, const char *filename) {
-    struct applicationLayer appLayer;
-    LinkLayerRole linkRole;
-    linkRole = (strcmp(role, "tx") == 0) ? LlTx : LlRx;
-    appLayer.status = (strcmp(role, "tx") == 0) ? 1 : 0;
-    LinkLayer linkLayer;
-    linkLayer.baudRate = baudRate;
-    linkLayer.nRetransmissions = retries;
-    linkLayer.role = linkRole;
-    strcpy(linkLayer.serialPort, port);
-    linkLayer.timeout = timeout;
-    int fd = llopen(linkLayer);
-    if (fd == -1)
-        return;
-    appLayer.fileDescriptor = fd;
-    switch (appLayer.status)
-    {   case 1:
-            printf("Sending file\n");
-            sendPacket(fd, START_PACKET, filename);
-            sendPacket(fd, DATA_PACKET, filename);
-            sendPacket(fd, END_PACKET, filename);
-            break;
-        case 0:
-            printf("Receiving file\n");
-            receivePacket(fd, filename);
-            break;
-        default:
-            printf("Invalid role\n");
-    }
-
-    printf("END\n");
-    llclose(0, linkLayer);}
-
-int sendPacket(int fd, unsigned char packetType, const char *filename) 
-{
-    switch (packetType) {
-        case START_PACKET:
-        case END_PACKET:
-            return sendControlPacket(fd, packetType, filename);
-        case DATA_PACKET:
-            return sendDataPacket(fd, filename);
-
-        default:
-            return -1;
-    }
-}
 
 int sendControlPacket(int fd, unsigned char packetType, const char *filename) 
 {
@@ -127,3 +88,51 @@ int receivePacket(int fd, const char *filename)
     fclose(file);
     return fd;
 }
+
+int sendPacket(int fd, unsigned char packetType, const char *filename) 
+{
+    switch (packetType) {
+        case START_PACKET:
+        case END_PACKET:
+            return sendControlPacket(fd, packetType, filename);
+        case DATA_PACKET:
+            return sendDataPacket(fd, filename);
+
+        default:
+            return -1;
+    }
+}
+
+void applicationLayer(const char *port, const char *role, int baudRate,
+                      int retries, int timeout, const char *filename) {
+    struct applicationLayer appLayer;
+    LinkLayerRole linkRole;
+    linkRole = (strcmp(role, "tx") == 0) ? LlTx : LlRx;
+    appLayer.status = (strcmp(role, "tx") == 0) ? 1 : 0;
+    LinkLayer linkLayer;
+    linkLayer.baudRate = baudRate;
+    linkLayer.nRetransmissions = retries;
+    linkLayer.role = linkRole;
+    strcpy(linkLayer.serialPort, port);
+    linkLayer.timeout = timeout;
+    int fd = llopen(linkLayer);
+    if (fd == -1)
+        return;
+    appLayer.fileDescriptor = fd;
+    switch (appLayer.status)
+    {   case 1:
+            printf("Sending file\n");
+            sendPacket(fd, START_PACKET, filename);
+            sendPacket(fd, DATA_PACKET, filename);
+            sendPacket(fd, END_PACKET, filename);
+            break;
+        case 0:
+            printf("Receiving file\n");
+            receivePacket(fd, filename);
+            break;
+        default:
+            printf("Invalid role\n");
+    }
+
+    printf("END\n");
+    llclose(0, linkLayer);}
