04fc64af39ef01f986a1aa3707712cfa0f656c02
Rúben Silva <rubensilva2003@gmail.com>
2023-11-06 20:17:57 +0000

    Removed time counts in main and added report

--- Proj1/main.c
+++ Proj1/main.c
@@ -42,19 +42,8 @@ int main(int argc, char *argv[])
            TIMEOUT,
            filename);
 
-    clock_t start, end;
-    double cpu_time_used;
-     
-     start = clock();
-
-   
+    
     applicationLayer(serialPort, role, BAUDRATE, N_TRIES, TIMEOUT, filename);
-
-    end = clock();
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC * 1000;
-    printf("Transfer took %.4f milliseconds to execute \n", cpu_time_used); 
-
-
 
     return 0;
 }
